d82d4461a22812c4bb05db7e9f251d349841129d
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2023-08-16 13:15:50 -0300

    Update api.ts

--- api.ts
+++ api.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseUrl =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3333'
-    : 'https://api.duckluckie.fun';
+    : process.env.NEXT_PUBLIC_API_URL;
 
 export const api = axios.create({
     baseURL: baseUrl,
